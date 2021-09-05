fc7e7641b1b73e849686b96e4f765304bf78cb69
Abiria <abiria.dev@gmail.com>
2021-09-06 05:28:17 +0900

    bot

--- index.js
+++ index.js
@@ -1,4 +1,7 @@
 const express = require("express");
+const discord = require("discord.js");
+
+const { FLAGS } = discord.Intents;
 
 const app = express();
 
@@ -9,3 +12,34 @@ app.get("/", (req, res) => {
 app.listen(process.env.PORT || 3000, () => {
   console.log("LISTENING");
 });
+
+const bot = new discord.Client({
+  intents: [
+    FLAGS.GUILDS,
+    FLAGS.GUILD_MEMBERS,
+    FLAGS.GUILD_BANS,
+    FLAGS.GUILD_EMOJIS_AND_STICKERS,
+    FLAGS.GUILD_INTEGRATIONS,
+    FLAGS.GUILD_WEBHOOKS,
+
+    FLAGS.GUILD_INVITES,
+    FLAGS.GUILD_VOICE_STATES,
+    FLAGS.GUILD_PRESENCES,
+    FLAGS.GUILD_MESSAGES,
+    FLAGS.GUILD_MESSAGE_REACTIONS,
+    FLAGS.GUILD_MESSAGE_TYPING,
+    FLAGS.DIRECT_MESSAGES,
+    FLAGS.DIRECT_MESSAGE_REACTIONS,
+    FLAGS.DIRECT_MESSAGE_TYPING,
+  ],
+});
+
+bot.on("ready", () => {
+  console.log(`loged in ${bot.user.tag}`);
+});
+
+bot.on("messageCreate", async (msg) => {
+  if (msg.author.id === "662201438621138954") msg.reply("dd");
+});
+
+bot.login();
